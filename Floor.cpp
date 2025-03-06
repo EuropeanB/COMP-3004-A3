9cd933674cf1fbcbd746fa2b0cb3ff5567874d6f
EuropeanB <55248830+EuropeanB@users.noreply.github.com>
2025-03-06 00:32:02 -0500

    Delete Floor.cpp

--- Floor.cpp
+++
@@ -1,8 +0,0 @@
-#include "Floor.h"
-
-Floor::Floor(int floorNumber, QObject *parent) : QObject(parent), floorNumber(floorNumber), upRequest(false), downRequest(false) {}
-
-void Floor::pressButton(bool up) {
-    if (up) upRequest = true;
-    else downRequest = true;
-}
